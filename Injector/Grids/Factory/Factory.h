3cd067ecc2a5e347a4742a7ff97ac88cf8c6b4ce
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-04-10 15:22:57 +0300

    grid factory is introduced

---
+++ Injector/Grids/Factory/Factory.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+#include <Injector/Grids/Defines.h>
+#include <Injector/Grids/Grids.h>
+
+namespace GPN
+{
+    namespace Grids
+    {
+        struct Factory
+        {
+            static auto generate_z_stencils_uniform(float_t a, float_t b, ptrdiff_t n)
+            {
+                float_t step{(b - a) / (n - 1)};
+
+                std::vector<float_t> result(n);
+
+                result[0] = a;
+                for (ptrdiff_t idx{1}; idx < n; ++idx)
+                    result[idx] = result[idx - 1] + step;
+
+                return result;
+            }
+        };
+    }
+}
