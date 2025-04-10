1fc1c7775fa73413d8100ac592f1657de54ba81d
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-04-10 10:23:01 +0300

    test template file added

---
+++ tests/grid_test.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+#include <Injector/Grids/Grids.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace GPN;
+
+// Tests Cartesian grid
+TEST_CASE("GridTest", "CartesianCoordinate")
+{
+}
+
+TEST_CASE("GridTest", "RadialCylinderCoordinate")
+{
+    
+}
+
+// int main(int argc, char **argv) 
+// {
+//     testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
