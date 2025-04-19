19f5db23556cf84dd1933e25bb817c0463f561b2
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-04-19 16:23:29 +0300

    refactored

--- Injector/Solver/State2D.hpp
+++ Injector/Solver/State2D.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <Eigen/Core>
-#include <Eigen/Dense>
+// #include <Eigen/Dense>
 
 #include <Injector/Grids/Defines.h>
-// #include <Injector/Grids/UniformGrid2D.h>
 
 namespace GPN
 {
@@ -23,48 +22,58 @@ namespace GPN
                 {
                 }
 
-                template<typename StructuredGrid2D_t>
-                static State2D FillWithZeros(
+                template <typename StructuredGrid2D_t>
+                static auto FillWithZeros(
                     const StructuredGrid2D_t &grid)
                 {
+                    return FillWithConst(grid, (Real_Type)0.0);
+                }
+
+                template <typename StructuredGrid2D_t>
+                static auto FillWithConst(
+                    const StructuredGrid2D_t &grid,
+                    RealType val)
+                {
                     State_Container cur_state{
                         grid.first_coord.size(),
                         grid.second_coord.size()};
-                    cur_state.fill(0.0);
+                    cur_state.fill(val);
 
-                    return {cur_state};
+                    return State2D{cur_state};
                 }
 
-                template<
-                    typename StructuredGrid2D_t, 
-                    typename Functor
-                >
-                static State2D FillWithFunctor(
-                    const StructuredGrid2D_t &grid, 
-                    const Functor &f, RealType initial_moment)
+                template <
+                    typename StructuredGrid2D_t,
+                    typename Functor>
+                static auto FillWithFunctor(
+                    const StructuredGrid2D_t &grid,
+                    const Functor &f, RealType initial_moment = 0.0)
                 {
                     State_Container cur_state{
                         grid.first_coord.size(),
                         grid.second_coord.size()};
 
-                    for (ptrdiff_t j = 0; j < cur_state.outerSize(); ++j)
-                        for (ptrdiff_t i = 0; i < cur_state.innerSize(); ++i)
+                    for (std::ptrdiff_t j = 0; j < cur_state.outerSize(); ++j)
+                        for (std::ptrdiff_t i = 0; i < cur_state.innerSize(); ++i)
                         {
-                            cur_state(i, j) = f(grid.first_coord[i], grid.second_coord[j], initial_moment);
+                            cur_state(i, j) = f(
+                                grid.first_coord[i],
+                                grid.second_coord[j],
+                                initial_moment);
                         }
 
-                    return {cur_state};
+                    return State2D{cur_state};
                 }
 
                 State2D(const State2D &) noexcept = default;
                 State2D(State2D &&) noexcept = default;
 
-                RealType operator()(ptrdiff_t i, ptrdiff_t j) const
+                RealType operator()(auto i, auto j) const
                 {
                     return cur_state(i, j);
                 }
 
-                RealType &operator()(ptrdiff_t i, ptrdiff_t j)
+                RealType &operator()(auto i, auto j)
                 {
                     return cur_state(i, j);
                 }
@@ -72,7 +81,6 @@ namespace GPN
             public:
                 State_Container cur_state;
             };
-
         } // Coefficients
     } // EqSolver
 } // GPN
