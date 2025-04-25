fb2de70c1e6435428956614c0f08684731c2a792
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-04-25 15:52:08 +0300

    fixes

--- Injector/Grids/ConcreteGrids.hpp
+++ Injector/Grids/ConcreteGrids.hpp
@@ -7,10 +7,14 @@ namespace GPN
 {
     namespace Grids
     {
+        /// @brief Concrete grid along radial direction of cylinder system of coordinates
         struct RGrid : public AxesGrid<CoordinateTypes::R_CylCoord>{};
+        /// @brief Concrete grid along Z direction of Cartesian system of coordinates
         struct ZGrid : public AxesGrid<CoordinateTypes::Z>{};
 
+        /// @brief Concrete grid along X direction of Cartesian system of coordinates
         struct XGrid : public AxesGrid<CoordinateTypes::X>{};
+        /// @brief Concrete grid along Y direction of Cartesian system of coordinates
         struct YGrid : public AxesGrid<CoordinateTypes::Y>{};
     } // Grids
 
--- Injector/Properties/Factory.hpp
+++ Injector/Properties/Factory.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include <Injector/Grids/Grids1D.hpp>
 #include <Injector/Grids/Grids2D.hpp>
 #include <Injector/Properties/Logs.hpp>
 #include <Injector/Properties/PhysicalField.hpp>
