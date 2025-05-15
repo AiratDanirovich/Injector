385c6b0ade7d73049890973500034f20fd828b67
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-05-15 14:46:32 +0300

    ExternalPressure log added

--- Injector/Properties/Logs.hpp
+++ Injector/Properties/Logs.hpp
@@ -210,6 +210,24 @@ namespace GPN
             using IndicatorProperty::IndicatorProperty;
         };
 
+        struct ExternalPressure
+            : public StepPropertyGrid,
+              private AssertNonNegative
+        {
+            ExternalPressure(
+                const StepPropertyGrid &pressure,
+                const IsPermeable &is_permeable)
+                : StepPropertyGrid{pressure},
+                  AssertNonNegative{pressure}
+            {
+                assert(pressure.size() == is_permeable.size());
+                for (std::ptrdiff_t id{0ll}; id < pressure.size(); ++id)
+                    assert(
+                        ((is_permeable(id) == 1.0) && (pressure(id) > 0.0)) ||
+                        ((is_permeable(id) == 0.0) && (pressure(id) == 0.0)));
+            }
+        };
+
         struct Permeability
             : public StepPropertyGrid,
               private AssertNonNegative
