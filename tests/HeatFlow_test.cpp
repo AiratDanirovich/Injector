5c32e11d6bcd6b3026b682a087c1b9ec9b6cd5be
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-05-24 18:54:43 +0300

    fix

--- tests/HeatFlow_test.cpp
+++ tests/HeatFlow_test.cpp
@@ -1,6 +1,7 @@
 // #include <cmath>
 #include <memory>
 #include <fstream>
+#include <string>
 
 #include <Injector/Grids/Defines.h>
 
@@ -67,11 +68,14 @@ struct FunctorBC : public BoundaryConditions::BCFunctorBase
 
   RealType operator()(RealType z, RealType r, RealType t) const override
   {
-    if (z == grid_ptr->first_coord.dual_front())
-    {
-      if (r == grid_ptr->second_coord.mesh_front())
-        return fluid.volumetric_heat_capacity * flow_field.axes1_as_face_normal(0, 0) * inlet_temp;
-    }
+    //   if (z == grid_ptr->first_coord.dual_front())
+    //   {
+    //     if (r == grid_ptr->second_coord.mesh_front())
+    //       return fluid.volumetric_heat_capacity * flow_field.axes1_as_face_normal(0, 0) * inlet_temp;
+    //   }
+
+    if (r == grid_ptr->second_coord.dual_front())
+      return fluid.volumetric_heat_capacity * flow_field.axes2_as_face_normal(0, 0) * inlet_temp;
 
     return 0.0;
   }
@@ -86,39 +90,39 @@ protected:
 using VR = std::vector<RealType>;
 
 /*START*/
-  // input parameters
-  /*heat rate*/
-  RealType q{1.0};
-  /*fluid*/
-  RealType viscosity{6e-4}, density{1000}, capacity{4200};
-  /*collector*/
-  const RealType rMin{1.0}, rMax{2.0}, zTop{0.0};
-  const std::ptrdiff_t rNodes{301ull};
-  const std::ptrdiff_t nLayers{11ull};
-  const VR thickness(nLayers, 1); // each layer is 1m thick
+// input parameters
+/*heat rate*/
+RealType q{1.0};
+/*fluid*/
+RealType viscosity{6e-4}, density{1000}, capacity{4200};
+/*collector*/
+const RealType rMin{100.0}, rMax{101.0}, zTop{0.0};
+const std::ptrdiff_t rNodes{31ull};
+const std::ptrdiff_t nLayers{11ull};
+const VR thickness(nLayers, 1); // each layer is 1m thick
 
-  // hydrodynamic logs
-  const VR is_permeable(nLayers, 1.0);
-  const VR porosity(nLayers, 1e-16);
-  const VR permeability(nLayers, 0.5);
-  const VR ext_pressure(nLayers, 13E6);
-  const VR skin(nLayers, 0.0);
+// hydrodynamic logs
+const VR is_permeable(nLayers, 1.0);
+const VR porosity(nLayers, 1.0);
+const VR permeability(nLayers, 0.5);
+const VR ext_pressure(nLayers, 13E6);
+const VR skin(nLayers, 0.0);
 
-  // heat logs
-  const VR conductivity(nLayers, 3.9);
-  const VR solid_density(nLayers, 3.9 /*should be 2600 in SI*/);
-  const VR solid_specific_heatcapacity(nLayers, 1.0 /*should be 770 in SI*/);
-  /*temporal grid*/
-  const std::ptrdiff_t time_steps_nmbr{501ull};
-  const RealType t0{1.0}; // initial time moment
-  const RealType t1{t0 + 1.0};
-  const RealType time_step{(t1 - t0) / time_steps_nmbr};
-  const VR time_intervals(time_steps_nmbr, time_step);
-  /*temperatures*/
-  const RealType well_rate{1.0};
-  const RealType initial_temperature{0.0};
-  const RealType inlet_temperature{1.0};
-  /*END*/
+// heat logs
+const VR conductivity(nLayers, 3.9);
+const VR solid_density(nLayers, 3.9 /*should be 2600 in SI*/);
+const VR solid_specific_heatcapacity(nLayers, 1.0 /*should be 770 in SI*/);
+/*temporal grid*/
+const std::ptrdiff_t time_steps_nmbr{5ull};
+const RealType t0{1.0}; // initial time moment
+const RealType t1{t0 + 0.01};
+const RealType time_step{(t1 - t0) / time_steps_nmbr};
+const VR time_intervals(time_steps_nmbr, time_step);
+/*temperatures*/
+const RealType well_rate{0.01};
+const RealType initial_temperature{0.0};
+const RealType inlet_temperature{1.0};
+/*END*/
 
 TEST_CASE("Solver", "SelfSimilarCyl")
 {
@@ -160,12 +164,14 @@ TEST_CASE("Solver", "SelfSimilarCyl")
           solid_density, solid_specific_heatcapacity,
           water, grid_factory.grid())};
 
-  const Well_KH well{
-      Phases::FluidFactory::create_water(0.0, 0.0),
-      hydro_logs_factory.is_permeable,
-      hydro_logs_factory.permeability};
+  // const Well_KH well{
+  //     Phases::FluidFactory::create_water(0.0, 0.0),
+  //     hydro_logs_factory.is_permeable,
+  //     hydro_logs_factory.permeability};
   Properties::ReservoirFlowField flow_field{
-      well_rate, well, *grid_factory.grid()};
+    Properties::FlowFactory::horizontal_flow(well_rate*capacity_field.value(0,0), hydro_logs_factory.is_permeable, *grid_factory.grid())
+    //  well_rate, well, *grid_factory.grid()
+    };
 
   // initial condition
   const auto initial_state{ICFactory(t0, grid_factory.grid(), initial_temperature)};
@@ -192,8 +198,41 @@ TEST_CASE("Solver", "SelfSimilarCyl")
   std::string pathz{"data_z.csv"};
 
   // assert solution
+  const double tol = 1E-15;
   for (size_t t_step{0ll}; t_step < time_intervals.size(); ++t_step)
   {
-    solver.advance(time_intervals[t_step]);
+    // for (auto i{0ull}; i < matricies.mx.size(); ++i)
+    // {
+    //   const auto &m = matricies.mx[i];
+    //   REQUIRE(m.rows() == m.cols());
+    //   {
+    //     auto col{0ll};
+    //     INFO("" << "col: " << col << ", c: " << m.coeff(col, col) << ", l: " << 0.0 << ", r: " << m.coeff(col, col + 1ll));
+    //     CHECK_THAT(-m.coeff(col, col), WithinRel(m.coeff(col, col + 1ll), tol));
+    //   }
+    //   for (auto col{1ll}; col < m.cols() - 1; ++col)
+    //   {
+    //     INFO("" << "col: " << col << ", c: " << m.coeff(col, col) << ", l: " << m.coeff(col, col - 1ll) << ", r: " << m.coeff(col, col + 1ll));
+    //     CHECK_THAT(-m.coeff(col, col), WithinRel(m.coeff(col, col - 1ll) + m.coeff(col, col + 1ll), tol));
+    //     INFO("" << "col: " << col << ", c: " << m.coeff(col, col));
+    //     CHECK(m.coeff(col, col) > tol);
+    //   }
+    //   {
+    //     auto col{m.cols() - 1ll};
+    //     INFO("" << "col: " << col << ", c: " << m.coeff(col, col) << ", l: " << m.coeff(col, col - 1ll) << ", r: " << 0.0);
+    //     CHECK_THAT(-m.coeff(col, col), WithinRel(m.coeff(col, col - 1ll), tol));
+    //   }
+    // }
+  }
+
+  const auto &[times, states] = solver.solution();
+
+  for (auto i{0ull}; i < times.size(); ++i)
+  {
+    std::string path{std::string{"T_"} + std::to_string(i) + std::string{".txt"}};
+    std::ofstream f{path};
+
+    f << states[i].cur_state;
+    f.close();
   }
 }
