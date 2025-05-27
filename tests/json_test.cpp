b1cc79243d50379457a4d6d42169ecde988ec4f5
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-05-27 20:40:31 +0300

    nlohmann_json libbrary is available and simple test for the header avalability is introduced

---
+++ tests/json_test.cpp
@@ -0,0 +1,13 @@
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+#include <catch2/catch_test_macros.hpp>
+
+
+
+// Tests Cylinder grid, (r; z)
+TEST_CASE("BaseSplitTest")
+{
+    
+}
