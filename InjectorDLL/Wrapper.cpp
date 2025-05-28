821afa792a590e625313227f922902fd9e82b7a6
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-05-28 18:35:57 +0300

    fix

--- InjectorDLL/Wrapper.cpp
+++ InjectorDLL/Wrapper.cpp
@@ -1,16 +1,22 @@
-
 #include <vector>
+#include <cmath>
+#include <memory>
+#include <cassert>
 
 #include <InjectorDLL/Wrapper.h>
 
-#include <Injector/Grids/Factory.hpp>
-#include <Injector/Grids/PhysicalField.hpp>
-#include <Injector/Properties/Factory.hpp>
+#include <Injector/Grids/Grids2D.hpp>
+
+#include <Injector/Properties/Logs.hpp>
+#include <Injector/Properties/FieldsFactory.hpp>
+#include <Injector/Properties/FlowField.hpp>
 
 #include <Injector/Model/Phases/FluidFactory.hpp>
+#include <Injector/Model/Collector.hpp>
 
+#include <Injector/Solver/BoundaryConditions.hpp>
+#include <Injector/Solver/InitialCondition.hpp>
 #include <Injector/Solver/SplittingMethod/Solver.hpp>
-
 
 using namespace GPN;
 using namespace GPN::Phases;
@@ -19,9 +25,10 @@ using namespace GPN::EqSolver::SplittingMethod;
 
 struct ExactSolution
 {
+    using Grid2D_t = Grids::StructuredCylinderGrid2DAxisymmetric;
     ExactSolution(
-        const Properties::HeatVolumetricCapacity &volumetric_capacity,
-        const Properties::HeatConductivity &heat_conductivity,
+        const Properties::MediumHeatVolumetricCapacity<Grid2D_t> &volumetric_capacity,
+        const Properties::HeatConductivity<Grid2D_t> &heat_conductivity,
         RealType q)
         : kappa{volumetric_capacity, heat_conductivity},
           heat_conductivity{heat_conductivity},
@@ -44,10 +51,10 @@ struct ExactSolution
     }
 
 protected:
-    const Properties::ThermalDiffusivity kappa;
-    const Properties::HeatConductivity &heat_conductivity;
+    const Properties::ThermalDiffusivity<Grid2D_t> kappa;
+    const Properties::HeatConductivity<Grid2D_t> &heat_conductivity;
     const RealType q;
-    const Properties::HeatConductivity::Grid_type &grid;
+    const Grid2D_t &grid;
 };
 
 struct FunctorIC
@@ -71,19 +78,46 @@ auto initialcondition_factory(RealType t0, const Grid_t_ptr grid, const ExactSol
     return State::State2D{State::State2D::FillWithFunctor(*grid, FunctorIC{es}, t0)};
 }
 
-struct FunctorBC : public GPN::BoundaryConditions::BCFunctorBase
+struct AFunctorBC : public GPN::BoundaryConditions::BCFunctorBase
 {
-    FunctorBC(const ExactSolution &es)
-        : es{es}
+    using Grid2D_t = Grids::StructuredCylinderGrid2DAxisymmetric;
+    AFunctorBC(
+        const ExactSolution &es,
+        const cptr<Grid2D_t> grid2D)
+        : es{es},
+          grid2D{grid2D}
     {
     }
-    RealType operator()(RealType z, RealType r, RealType t) const override
+
+    RealType operator()(ptrdiff_t z_id, RealType r, RealType t) const override
     {
+        RealType z{grid2D->first_coord.mesh_nodes(z_id)};
+        if (r == grid2D->second_coord.dual_front())
+            r = grid2D->second_coord.mesh_front();
+        else if (r == grid2D->second_coord.dual_back())
+            r = grid2D->second_coord.mesh_back();
+        else
+            assert(false);
+
+        return es(z, r, t);
+    }
+
+    RealType operator()(RealType z, ptrdiff_t r_id, RealType t) const override
+    {
+        RealType r{grid2D->second_coord.mesh_nodes(r_id)};
+        if (z == grid2D->first_coord.dual_front())
+            z = grid2D->first_coord.mesh_front();
+        else if (z == grid2D->first_coord.dual_back())
+            z = grid2D->first_coord.mesh_back();
+        else
+            assert(false);
+
         return es(z, r, t);
     }
 
 protected:
     const ExactSolution &es;
+    const cptr<Grid2D_t> grid2D;
 };
 
 Wrapper::Wrapper(
@@ -101,9 +135,9 @@ Wrapper::Wrapper(
     // six vectors of the same size
     // values are in SI
     const VR &thickness,                   // meter
-    const VR &conductivity,                // Watt/(m*K)
-    const VR &porosity,                    // --
-    const VR &is_permeable,                // {0, 1}, --
+    const VR &heatconductivity_stencils,   // Watt/(m*K)
+    const VR &porosity,                    // 0.0 < porosity <= 1.0, --
+    const VR &is_permeable_stencils,       // {0, 1}, --
     const VR &solid_density,               // kg/(m^3)
     const VR &solid_specific_heatcapacity, // J/(kg*K)
     // temporal grid
@@ -112,6 +146,14 @@ Wrapper::Wrapper(
     const VR &time_intervals // intervals of const rates)
 )
 {
+    // adapt stl container to Eigne conteiner
+    LogValuesContainer solid_density_stencils(solid_density.size());
+    std::copy(solid_density.begin(), solid_density.end(), solid_density_stencils.begin());
+    LogValuesContainer solid_specific_heatcapacity_stencils(solid_specific_heatcapacity.size());
+    std::copy(solid_specific_heatcapacity.begin(), solid_specific_heatcapacity.end(), solid_specific_heatcapacity_stencils.begin());
+    LogValuesContainer porosity_stencils(porosity.size());
+    std::copy(porosity.begin(), porosity.end(), porosity_stencils.begin());
+
     // make grid1D
     const VR z_stencils(
         Grids::Factory::generate_dual_grid_stencils_from_steps(
@@ -120,40 +162,52 @@ Wrapper::Wrapper(
     const VR r_stencils{
         Grids::Factory::generate_dual_grid_stencils_uniform(
             Segment{rMin, rMax}, r_nodes_nmbr)};
-    // make grid2D
-    const auto grid2D{
-        Grids::Factory::create_cylinder_grid_2D_ptr(
-            z_stencils, r_stencils)};
-    // heat conductivity
-    const Properties::HeatConductivity conductivity_field{
-        Properties::Factory::generate_heatconductivity_Property(
-            conductivity, grid2D)};
+    const auto grid2D{Grids::CylinderGridFactory::create(z_stencils, r_stencils)};
+    const auto &grid{grid2D->first_coord};
     // make fluid
     const Water water{
         FluidFactory::create_water(
             Viscosity{viscosity},
             Density{density},
             SpecificHeatCapacity{capacity})};
-    // volumetric heat capacity of multiphaase system
-    const Properties::HeatVolumetricCapacity capacity_field{
-        Properties::Factory::generate_volumetric_heatcapacity_Property(
-            is_permeable, porosity,
-            solid_density, solid_specific_heatcapacity,
-            water, grid2D)};
+    // time moments
+    const VR t_stencils(
+        Grids::Factory::generate_dual_grid_stencils_from_steps(
+            t_start, time_intervals));
+    // solver
+    const Logs::Rocks::IsPermeableLog core_data{
+        is_permeable_stencils,
+        grid};
+    const auto flow_field{
+        FaceProperties::FlowFactory::zero_flow(
+            core_data.is_permeable, *grid2D)};
+
+    const Logs::Rocks::HeatLogs heat_logs{
+        solid_density_stencils,
+        solid_specific_heatcapacity_stencils,
+        heatconductivity_stencils,
+        porosity_stencils,
+        Phases::FluidFactory::create_water(1.0, 1.0),
+        grid};
+    const Properties::Rocks::HeatProps heat_props{
+        heat_logs, grid2D};
+
+    const FaceProperties::Rocks::HeatFaceProps heat_face_props{
+        heat_props, grid2D};
 
     // exact solution
-    ExactSolution es{capacity_field,
-                     conductivity_field, q};
+    ExactSolution es{heat_props.medium_vol_heatcapacity,
+                     heat_props.heat_conductivity, q};
     // initial conditions
     const auto initial_state{initialcondition_factory(t_start, grid2D, es)};
     // boundary conditions
     const GPN::BoundaryConditions::BoundaryConditions bc{
-        *grid2D, std::make_shared<FunctorBC>(es)};
+        *grid2D, std::make_shared<AFunctorBC>(es, grid2D)};
 
-    // solver
     Solver solver{
-        conductivity_field, grid2D,
-        capacity_field,
+        heat_face_props.heat_conductivity,
+        flow_field, grid2D,
+        heat_props.medium_vol_heatcapacity,
         initial_state,
         bc, t_start};
 
@@ -164,7 +218,7 @@ Wrapper::Wrapper(
 
         const auto &[t, sol] = solver.solution().back();
         // save time
-       time.push_back(t);
+        time.push_back(t);
         // save T
         t_radial_distribution.push_back(std::vector<RealType>(sol.cols(), -1001.0));
         // copy T(r) at z = (zTop + zBottom)/2.0
@@ -184,5 +238,4 @@ std::vector<std::vector<RealType>> Wrapper::get_temps() const
 
 Wrapper::~Wrapper()
 {
-
 }
