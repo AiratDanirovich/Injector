639970ce37959668304d271303031c342b526764
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-09-20 10:29:49 +0300

    verticle flux in the cement is implemented

--- tests/cross_flow_test.cpp
+++ tests/cross_flow_test.cpp
@@ -71,13 +71,31 @@ struct Well_CrossFlow
     {
         if (std::isnan(rate))
         { // define rate from pressure
-            throw std::invalid_argument("RFP: Rate must be set");
+            throw std::invalid_argument("WFP: Rate must be set");
         }
         else if (std::isnan(pressure))
         { // define pressure from rate
             assert(!std::isnan(rate));
             assert(rate >= 0.0);
             return ((rate / weights_sum) * WFP_weights).eval();
+        }
+        else
+            throw std::invalid_argument("WFP: Either rate or pressure must be set, but not both.");
+    }
+
+    StepPropertyContainer get_verticle_cement_flow(
+        RealType rate,
+        RealType pressure) const
+    {
+        if (std::isnan(rate))
+        { // define rate from pressure
+            throw std::invalid_argument("RFP: Rate must be set");
+        }
+        else if (std::isnan(pressure))
+        { // define pressure from rate
+            assert(!std::isnan(rate));
+            assert(rate >= 0.0);
+            return ((rate / weights_sum) * cross_flows.get_verticle_flux(rate)).eval();
         }
         else
             throw std::invalid_argument("RFP: Either rate or pressure must be set, but not both.");
