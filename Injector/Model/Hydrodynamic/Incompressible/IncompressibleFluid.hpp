3aa02e46a8645df27e4f84ed7b3f4db1ad34f100
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-09-30 13:07:26 +0300

    2D grid for pressure field is introduced

---
+++ Injector/Model/Hydrodynamic/Incompressible/IncompressibleFluid.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Injector/Solver/State2D.hpp>
+
+
+
+namespace GPN
+{
+    namespace Hydrodynamic
+    {
+        struct PressureField 
+        : public EqSolver::State::State2D
+        {
+
+        };
+
+
+    } // Hydrodynamic
+
+} // GPN
