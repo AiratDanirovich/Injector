c5eeb1b96889bc4f5c38b7972b4ed35649db2eea
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-01 20:44:00 +0300

    class development in progress

--- Injector/Model/Hydrodynamic/Incompressible/IncompressibleFluid.hpp
+++ Injector/Model/Hydrodynamic/Incompressible/IncompressibleFluid.hpp
@@ -12,24 +12,52 @@ namespace GPN
         struct PressureField
             : public EqSolver::State::State2D
         {
+            PressureField(const EqSolver::State::State2D &state)
+                : EqSolver::State::State2D{state}
+            {
+            }
+
+            /// @brief Initial pressure is assumed to be constant with r
+            struct FunctorIC : public GPN::InitialConditions::ICFunctorBase
+            {
+                FunctorIC(const Logs::ExternalPressure &ext_pressure)
+                    : ext_pressure{ext_pressure}
+                {
+                }
+                RealType operator()(const ptrdiff_t z_id, const ptrdiff_t, GPN::RealType) const override
+                {
+                    return ext_pressure(z_id);
+                }
+
+            protected:
+                const Logs::ExternalPressure &ext_pressure;
+            };
+
+            template <typename Grid_t_ptr>
+            static auto ICFactory(RealType t0, const Grid_t_ptr grid, const Logs::ExternalPressure &ext_pressure)
+            {
+                return EqSolver::State::State2D{EqSolver::State::State2D::FillWithFunctor(*grid, FunctorIC{ext_pressure}, t0)};
+            }
         };
 
         template <typename Grid2D_t, typename Fluid_t>
         struct IncompressibleFluidField
         {
             IncompressibleFluidField(
+                const RealType start_time,
                 const Fluid_t &fluid,
                 const Logs::Permeability &permeability,
                 const Logs::ExternalPressure &ext_pressure,
                 const cptr<Grid2D_t> grid2D)
                 : fluid{fluid},
                   permeability{permeability},
-                  ext_pressure{ext_pressure}
+                  ext_pressure{ext_pressure},
+                  P{PressureField::ICFactory(start_time, grid2D, ext_pressure)}
             {
             }
 
         private:
-            //            PressureField P;
+            PressureField P;
 
             const Fluid_t fluid;
             const cptr<Grid2D_t> grid2D;
--- tests/incompressible_fluid_field_test.cpp
+++ tests/incompressible_fluid_field_test.cpp
@@ -62,9 +62,11 @@ TEST_CASE("Solver", "SelfSimilarCyl")
       z_minor_step{data["grid"]["z_minor_step"].get<RealType>()}; // m
   const auto thickness{data["collector"]["thickness"].get<VR>()};
   const auto permeability_stencils{transfer_to_eigen(data["collector"]["permeability"].get<VR>(), 1e-12)};
-  const auto ext_pressure_stencils{transfer_to_eigen(data["collector"]["external_pressure"].get<VR>(), 0.0)};
+  const auto ext_pressure_stencils{transfer_to_eigen(data["collector"]["external_pressure"].get<VR>())};
   const auto is_perforated_stencils{transfer_to_eigen(data["collector"]["is_perforated"].get<VR>())};
   const auto is_permeable_stencils{set_is_permeable_stencils(is_perforated_stencils, to_layers)};
+
+  const auto start_time{data["history"]["start_time"].get<RealType>()};
 
   /*completion*/
   // z-refiner
@@ -131,6 +133,7 @@ TEST_CASE("Solver", "SelfSimilarCyl")
           JouleThomson{joule_thomson})};
 
   const auto pressure_field{IncompressibleFluidField{
+      start_time,
       water,
       permeability,
       external_pressure,
