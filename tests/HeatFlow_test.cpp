35886f9706d7b29a3b145ef2afe9c53d6676fed2
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-01 19:58:15 +0300

    IC and BC classes moved to other file

--- tests/HeatFlow_test.cpp
+++ tests/HeatFlow_test.cpp
@@ -24,8 +24,6 @@
 #include <Injector/Properties/FlowField.hpp>
 #include <Injector/Properties/Factory.hpp>
 #include <Injector/Solver/BoundaryConditions.hpp>
-#include <Injector/Solver/State2D.hpp>
-#include <Injector/Solver/InitialCondition.hpp>
 #include <Injector/Solver/FullImplicit/Solver.hpp>
 #include <Injector/Solver/SolverManager.hpp>
 
@@ -36,6 +34,7 @@
 #include "includes/get_completion.hpp"
 #include "includes/generate_stencils_and_steps.hpp"
 #include "includes/set_is_permeable_stencils.hpp"
+#include "includes/IC_BC.hpp"
 
 #include <nlohmann/json.hpp>
 #include <catch2/catch_test_macros.hpp>
@@ -54,75 +53,6 @@ using namespace GPN::Phases;
 using namespace GPN::Completion;
 using namespace GPN::EqSolver;
 using namespace GPN::EqSolver::FullImplicit;
-
-/// @brief Initial temperature is assumed to be constant
-struct FunctorIC : public InitialConditions::ICFunctorBase
-{
-  FunctorIC(const Logs::Geotherma &geotherma)
-      : geotherma{geotherma}
-  {
-  }
-  RealType operator()(const ptrdiff_t z_id, const ptrdiff_t, RealType) const override
-  {
-    return geotherma(z_id);
-  }
-
-protected:
-  const Logs::Geotherma &geotherma;
-};
-
-template <typename Grid_t_ptr>
-auto ICFactory(RealType t0, const Grid_t_ptr grid, const Logs::Geotherma &geotherma)
-{
-  return State::State2D{State::State2D::FillWithFunctor(*grid, FunctorIC{geotherma}, t0)};
-}
-
-template <typename Well_t>
-struct FunctorBC : public BoundaryConditions::BCFunctorBase
-{
-  using Grid2D_t = Grids::StructuredCylinderGrid2DAxisymmetric;
-  using ConvectionFieldFactory_t =
-      GPN::FaceProperties::RatesFactory<
-          Grid2D_t, Well_t, PhasePropertiesJT>;
-  FunctorBC(
-      const Logs::IsPermeable &is_permeable,
-      const ConvectionFieldFactory_t &flow_field, // volumetric heat flow rate
-      const cptr<const Grid2D_t> grid_ptr)
-      : flow_field{flow_field},
-        is_permeable{is_permeable},
-        grid_ptr{grid_ptr}
-  {
-  }
-
-  RealType operator()(const ptrdiff_t z_id, const RealType r, const RealType t) const override
-  {
-    if (r == grid_ptr->second_coord.dual_front())
-      return flow_field.get_heat_flow_in_axes2()(z_id, 0ll) * flow_field.get_temperature();
-
-    if (r == grid_ptr->second_coord.dual_back())
-      return 0.0;
-
-    assert(false);
-    return 0.0;
-  }
-
-  RealType operator()(const RealType z, const ptrdiff_t r_id, const RealType t) const override
-  {
-    if (z == grid_ptr->first_coord.dual_front())
-      return flow_field.get_heat_flow_in_axes1()(0ll, r_id) * flow_field.get_temperature();
-
-    if (z == grid_ptr->first_coord.dual_back())
-      return 0.0;
-
-    assert(false);
-    return 0.0;
-  }
-
-protected:
-  const Logs::IsPermeable &is_permeable;
-  const cptr<const Grid2D_t> grid_ptr;
-  const ConvectionFieldFactory_t &flow_field;
-};
 
 TEST_CASE("Solver", "SelfSimilarCyl")
 {
