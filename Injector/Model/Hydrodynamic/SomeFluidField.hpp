e55fc3bb4e9b8492b78291d339f6077a528543eb
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-10 09:36:26 +0300

    file added

---
+++ Injector/Model/Hydrodynamic/SomeFluidField.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <cassert>
+
+#include <Injector/Grids/Defines.h>
+
+#include <Injector/Properties/Logs.hpp>
+#include <Injector/Properties/PhysicalField.hpp>
+
+namespace GPN
+{
+    namespace Hydrodynamic
+    {
+        template <typename Grid2D_t, typename Fluid_t, typename Well_t>
+        struct SomeFluidField
+        {
+            SomeFluidField(
+                const RealType start_time,
+                const Fluid_t &fluid,
+                const Logs::Permeability &permeability,
+                const Logs::ExternalPressure &ext_pressure,
+                const Well_t &well,
+                const cptr<Grid2D_t> grid2D)
+                : fluid{fluid},
+                  permeability{permeability},
+                  ext_pressure{ext_pressure},
+                  P_ext{set_initial_pressure(ext_pressure, grid2D)},
+                  P{std::make_shared<Properties::Pressure<Grid2D_t>>(
+                      set_initial_pressure(ext_pressure, grid2D))},
+                  thickness_log{grid2D->first_coord.control_volumes},
+                  well{well},
+                  grid2D{grid2D},
+                  current_time{start_time}
+            {
+            }
+
+            const Properties::Pressure<Grid2D_t> &current_pressure() const
+            {
+                return *P;
+            }
+
+        public:
+            const Properties::Pressure<Grid2D_t> P_ext;
+            const Fluid_t &fluid;
+            const Well_t &well;
+            const cptr<Grid2D_t> grid2D;
+            const ControlVolumesContainer &thickness_log;
+            const Logs::Permeability &permeability;
+            const Logs::ExternalPressure &ext_pressure;
+
+        protected:
+            std::shared_ptr<Properties::Pressure<Grid2D_t>> P;
+            RealType current_time;
+
+            /// @brief Set the initial pressure replicating the pressure at the external boundary
+            /// @param ext_pressure Pressure log at the external boundary
+            /// @return Intial pressure field, const in r-direction
+            static auto set_initial_pressure(
+                const auto &ext_pressure,
+                const auto grid2D)
+            {
+                return Properties::FieldFactory::create(ext_pressure, grid2D);
+            }
+
+            template<typename HistoryRecord_t>
+            void set_time(
+                const RealType time_step, 
+                const HistoryRecord_t &history_record)
+            {
+                assert(time_step <= history_record.time_step);
+                current_time += time_step;
+            }
+        };
+
+    } // Hydrodynamic
+} // GPN
