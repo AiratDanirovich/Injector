220d6368f3790e7aa8f3dfb158af8ea239811d54
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-11 14:58:16 +0300

    files moved and added

--- Injector/Grids/GridMap/Grids2DMap.hpp
+++ Injector/Grids/GridMap/Grids2DMap.hpp
@@ -2,14 +2,14 @@
 
 #include <Eigen/Core>
 
+#include <Injector/Grids/GridMap/Grids1DMap.hpp>
 #include <Injector/Grids/Grids2D.hpp>
 
 namespace GPN
 {
     namespace Grids
     {
-
-        template <ptrdiff_t left_margin, typename Grid2D_t>
+        template<ptrdiff_t left_margin, typename Grid2D_t>
         struct Grid2DMap
         {
             Grid2DMap(const cptr<Grid2D_t> grid2D)
@@ -17,6 +17,11 @@ namespace GPN
             {}
 
         private:
+            using Axes1 = typename Grid2D_t::Axes_1;
+            using Axes2 = typename Grid2D_t::Axes_2;
+
+            const AxesGrid<Axes1> its_first_coord;
+            const AxesGridMap<left_margin, Axes2> its_second_coord;
             const cptr<Grid2D_t> grid2D;
         };
 
