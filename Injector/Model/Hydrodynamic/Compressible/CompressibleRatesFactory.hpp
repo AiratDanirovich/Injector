eb964864eaeaa9a3ebcdfa867f55df8bce6b7f52
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-27 10:36:28 +0300

    fix

--- Injector/Model/Hydrodynamic/Compressible/CompressibleRatesFactory.hpp
+++ Injector/Model/Hydrodynamic/Compressible/CompressibleRatesFactory.hpp
@@ -8,7 +8,6 @@
 
 #include <Injector/Properties/FlowField.hpp>
 
-
 namespace GPN
 {
     namespace FaceProperties
@@ -23,25 +22,29 @@ namespace GPN
 
             CompressibleRatesFactory(
                 cptr<Hydrodynamics_t> pressure_field,
-                const cptr<Grid2D_t> grid2D,
+                const cptr<Grid2D_t> grid2D_rocks,
                 const Well_t &well,
                 const ptr<History_t> history,
                 const Fluid_t &fluid)
-                : grid2D{grid2D},
+                : grid2D_rocks{grid2D_rocks},
+                  grid2D{*(grid2D_rocks->grid2D)}, 
                   well{well},
                   history{history},
                   fluid{fluid},
-                  pressure_field{pressure_field}
+                  pressure_field{pressure_field},
+                  first_size{
+                    grid2D_rocks->grid2D->first_coord().mesh_size()}, 
+                  second_size{
+                    grid2D_rocks->grid2D->second_coord().mesh_size()}
             {
             }
-
 
             /// @brief set the flow field at the next time moment
             /// @param t current time moment
             /// @param t_step step to the next time moment
             void set_flow_field(
                 double t, RealType t_step)
-            { 
+            {
                 // this method only works at FixedRate injection
                 assert(history->regime() == InjectorRegimes::FixedRate);
 
@@ -55,27 +58,46 @@ namespace GPN
                     std::make_shared<FaceProperties::HeatFlowField>(
                         // create ReservoirFlowField
                         FaceProperties::FlowFactory::create_from_well(
-                            history->get_current_record(), well, *grid2D),
+                            history->get_current_record(), well, *grid2D_rocks),
                         // multiple by heat capaity
                         fluid.volumetric_heat_capacity);
 
+                const FaceValuesContainer axes1_value{
+                    FaceValuesContainer::Zero(
+                        grid2D->first_coord().dual_size(),
+                        grid2D->second_coord().mesh_size())};
+                FaceValuesContainer axes2_value{
+                    FaceValuesContainer::Zero(
+                        grid2D->first_coord().mesh_size(),
+                        grid2D->second_coord().dual_size())};
+
+                axes2_value.leftCols(Grid2D_t::l_margin+1ll).colwise() = well.get_RFP(get_history_record());
+                
+            //    axes2_value.middleCols(second_size - Grid2D_t::l_margin) = rock_P;
+
+                heat_flow_field =
+                    std::make_shared<FaceProperties::HeatFlowField>(
+                        ReservoirFlowField{
+                            axes1_value,
+                            axes2_value},
+                        // multiple by heat capaity
+                        fluid.volumetric_heat_capacity);
             }
 
-
-
-
         public:
-            const cptr<Grid2D_t> grid2D;
+            const OriginalGrid &grid2D;
+            const cptr<Grid2D_t> grid2D_rocks;
             const Well_t &well;
             const ptr<History_t> history;
             const Fluid_t &fluid;
             ptr<Hydrodynamics_t> pressure_field;
 
         protected:
+            const ptrdiff_t first_size, second_size;
+
             cptr<FaceProperties::HeatFlowField> heat_flow_field;
             //    cptr<FaceProperties::ReservoirFlowField> volumetric_flow_field;
 
-            
             const auto get_history_record() const
             {
                 return history->get_current_record();
