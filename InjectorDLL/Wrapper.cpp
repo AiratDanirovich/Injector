ab6dd61b0368923e47b7a453ce5971b9d83f7cfa
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-28 12:08:06 +0300

    fix

--- InjectorDLL/Wrapper.cpp
+++ InjectorDLL/Wrapper.cpp
@@ -14,13 +14,13 @@
 #include <Injector/Grids/GridRefiners.hpp>
 
 #include <Injector/History/History.hpp>
-#include <Injector/History/RatesFactory.hpp>
 
 #include <Injector/Model/Phases/FluidFactory.hpp>
 #include <Injector/Model/Collector.hpp>
 #include <Injector/Model/Well/CrossFlow.hpp>
 #include <Injector/Model/Well/WellFactory.hpp>
 #include <Injector/Model/Hydrodynamic/Incompressible/IncompressibleFluid.hpp>
+#include <Injector/Model/Hydrodynamic/Incompressible/IncompressibleRatesFactory.hpp>
 #include <Injector/Model/Heat/HeatBoundaryConditions.hpp>
 #include <Injector/Model/Completion.hpp>
 #include <Injector/Model/ExtrudedCasingFactory.hpp>
