2304c4efc2abcef9e880360d531c691d019e4d16
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-10-31 17:54:25 +0300

    tests fixed

--- tests/compressible_fluid_field_test.cpp
+++ tests/compressible_fluid_field_test.cpp
@@ -197,12 +197,7 @@ TEST_CASE("Solver", "SelfSimilarCyl")
     for (auto t_step{0ll}; t_step < history->time_steps.size(); ++t_step)
     {
         history->advance();
-        const auto rfp{well.get_RFP(history->get_current_record())};
-        CHECK(rfp.rows() == grid_z.mesh_size());
-        const auto wfp{well.get_WFP(history->get_current_record())};
-        CHECK(wfp.rows() == grid_z.mesh_size());
-        const auto cement_flow{well.get_verticle_cement_flow(history->get_current_record())};
-        CHECK(cement_flow.rows() == grid_z.dual_size());
+        const RealType Q{history->rate()};
 
         const size_t internal_step_count{
             static_cast<size_t>(
@@ -212,10 +207,11 @@ TEST_CASE("Solver", "SelfSimilarCyl")
         for (size_t id{0ull}; id < internal_step_count; ++id, cur_time += step)
         {
             const auto t{cur_time + step};
-
             rates_factory.set_flow_field(cur_time, step);
+            const auto rfp{well.get_RFP(history->get_current_record())};
 #pragma region CHECK-PRESSURE
             const auto &P{pressure_field.current_pressure().values()};
+            CHECK(rfp.rows() == grid_z.mesh_size());
             for (auto row{0ll}; row < grid_z.mesh_size(); ++row)
             {
                 const auto rate{rfp(row)};
@@ -296,8 +292,15 @@ TEST_CASE("Solver", "SelfSimilarCyl")
             CHECK(flux2.rows() == grid_z.mesh_size());
             CHECK(flux2.cols() == grid_r.dual_size());
             const auto &mobility2{pressure_field.mobility.face_vals_axes2};
+            const auto wfp{well.get_WFP(history->get_current_record())};
+            CHECK(wfp.rows() == grid_z.mesh_size());
+            const auto cement_flow{well.get_verticle_cement_flow(history->get_current_record())};
+            CHECK(cement_flow.rows() == grid_z.dual_size());
+            const auto well_flow{well.get_verticle_well_flow(history->get_current_record())};
+            CHECK(well_flow.rows() == grid_z.dual_size());
+            RealType well_loss_cum_sum{0.0};
 #pragma region HORIZONTAL-RATES
-            for (auto row{0ll}; row < core_logs.is_permeable.size(); ++row)
+            for (auto row{0ll}; row < grid_z.mesh_size(); ++row)
             {
                 const auto k{core_logs.permeability(row)};
                 const auto h{grid_z.volume(row)};
@@ -358,27 +361,29 @@ TEST_CASE("Solver", "SelfSimilarCyl")
                     for (auto col{0ll}; col < grid_r.dual_size(); ++col)
                         CHECK(flux2(row, col) == 0.0);
                 }
-
             }
 #pragma endregion
 #pragma region VERTICAL-RATES
-                for(auto row{0ll}; row < grid_z.mesh_size(); ++row)
-                {
-                    {// flow in the tube
-                        const auto col{0ll};
-                        CHECK(false);
-                    }
-                    {// flow in the sandwich
-                        const auto col{1ll};
-                        CHECK(flux1(row, col) == 0.0);
-                    }
-                    {// flow in the cement
-                        const auto col{2ll};
-                        CHECK(flux1(row, col) == cement_flow(row));
-                    }
-                    for (auto col{3ll}; col < grid_r.mesh_size(); ++col)
-                        CHECK(flux1(row, col) == 0.0);
+            for (auto row{0ll}; row < grid_z.dual_size(); ++row)
+            {
+                { // flow in the tube
+                    const auto col{0ll};
+                    CHECK(Q - well_loss_cum_sum == well_flow(row));
+                    CHECK(C*(Q - well_loss_cum_sum) == flux1(row, col));
+                    if (row < grid_z.mesh_size())
+                        well_loss_cum_sum += wfp(row);
                 }
+                { // flow in the sandwich
+                    const auto col{1ll};
+                    CHECK(flux1(row, col) == 0.0);
+                }
+                { // flow in the cement
+                    const auto col{2ll};
+                    CHECK(flux1(row, col) == cement_flow(row));
+                }
+                for (auto col{3ll}; col < grid_r.mesh_size(); ++col)
+                    CHECK(flux1(row, col) == 0.0);
+            }
 #pragma endregion
 #pragma endregion
 #pragma region CHECK-JT
