df3b8a1e6da94dd2886d501def13dd1bfec84217
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-11-01 11:30:28 +0300

    merge fix

--- InjectorDLL/Wrapper.cpp
+++ InjectorDLL/Wrapper.cpp
@@ -253,9 +253,7 @@ Wrapper::Wrapper(
         decltype(CompressibleFluidField{
             start_time,
             water,
-            core_logs.permeability,
             rock_field_props,
-            base_hydrodynamics.ext_pressure,
             well,
             history,
             grid2D_rocks});
@@ -264,9 +262,7 @@ Wrapper::Wrapper(
         make_shared<FluidField_t>(
             start_time,
             water,
-            core_logs.permeability,
             rock_field_props,
-            base_hydrodynamics.ext_pressure,
             well,
             history,
             grid2D_rocks)};
