1da3de7836930a32b825cecac47454aec02e58d4
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-01-17 16:56:00 +0300

    text aligned

--- Injector/Solver/BoundaryConditions.hpp
+++ Injector/Solver/BoundaryConditions.hpp
@@ -18,8 +18,10 @@ namespace GPN
                 // sides of computational domain
                 enum Side
                 {
-                    East, West,
-                    North, South
+                    East,
+                    West,
+                    North,
+                    South
                 };
 
                 // types of boundary conditions
@@ -31,7 +33,7 @@ namespace GPN
                     third
                 };
             };
-            
+
             struct BCFunctorBase
             {
                 using BCType = BoundaryConditions::GeneralBC::BoundaryCondition::BCType;
@@ -48,39 +50,41 @@ namespace GPN
                 const cptr<Grid2D_t> &grid,
                 const cptr<const BCFunctorBase> functor,
                 BoundaryCondition::BCType bc_type = BoundaryCondition::undef)
-                : 
-            GeneralBC(
-                grid, functor,
-                std::array<BoundaryCondition::BCType, 4ull>{bc_type,bc_type,bc_type,bc_type})
+                : GeneralBC(
+                      grid, functor,
+                      std::array<BoundaryCondition::BCType, 4ull>{bc_type, bc_type, bc_type, bc_type})
             {
             }
-            
+
             template <typename Grid2D_t>
             GeneralBC(
                 const cptr<Grid2D_t> &grid,
                 const cptr<const BCFunctorBase> functor,
-                std::array<BoundaryCondition::BCType, 4ull> bc_types
-            )
-                :   bc_types{bc_types},
-                    fixed_coords{
-                        grid->first_coord().dual_front(),
-                        grid->first_coord().dual_back(),
-                        grid->second_coord().dual_front(), 
-                        grid->second_coord().dual_back()},
-                    functor{functor},
-                    t{0.0}
+                std::array<BoundaryCondition::BCType, 4ull> bc_types)
+                : bc_types{bc_types},
+                  fixed_coords{
+                      grid->first_coord().dual_front(),
+                      grid->first_coord().dual_back(),
+                      grid->second_coord().dual_front(),
+                      grid->second_coord().dual_back()},
+                  functor{functor},
+                  t{0.0}
             {
             }
 
             GeneralBC(const GeneralBC &) = default;
 
-            void set_bc_type(const RealType t) = delete;
+            // just update time t
+            void set_bc_type(const RealType t)
+            {
+                this->t = t;
+            }
 
-            template<typename MatrixView_t>
+            template <typename MatrixView_t>
             void set_west_val(MatrixView_t &view, const auto i) const
             {
                 const RealType y{fixed_coords[west_id]};
-                const BoundaryCondition::BCType 
+                const BoundaryCondition::BCType
                     bc_type{bc_types[west_id]};
                 if (bc_type == BoundaryCondition::BCType::first)
                 {
@@ -96,11 +100,11 @@ namespace GPN
                 assert(false && "West boundary condition is not properly set");
             }
 
-            template<typename MatrixView_t>
+            template <typename MatrixView_t>
             void set_east_val(MatrixView_t &view, const auto i) const
             {
                 const RealType y{fixed_coords[east_id]};
-                const BoundaryCondition::BCType 
+                const BoundaryCondition::BCType
                     bc_type{bc_types[east_id]};
                 if (bc_type == BoundaryCondition::BCType::first)
                 {
@@ -116,11 +120,11 @@ namespace GPN
                 assert(false && "East boundary condition is not properly set");
             }
 
-            template<typename MatrixView_t>
+            template <typename MatrixView_t>
             void set_south_val(MatrixView_t &view, const auto i) const
             {
                 const RealType x{fixed_coords[south_id]};
-                const BoundaryCondition::BCType 
+                const BoundaryCondition::BCType
                     bc_type{bc_types[south_id]};
                 if (bc_type == BoundaryCondition::BCType::first)
                 {
@@ -136,11 +140,11 @@ namespace GPN
                 assert(false && "South boundary condition is not properly set");
             }
 
-            template<typename MatrixView_t>
+            template <typename MatrixView_t>
             void set_north_val(MatrixView_t &view, const auto i) const
             {
                 const RealType x{fixed_coords[north_id]};
-                const BoundaryCondition::BCType 
+                const BoundaryCondition::BCType
                     bc_type{bc_types[north_id]};
                 if (bc_type == BoundaryCondition::BCType::first)
                 {
