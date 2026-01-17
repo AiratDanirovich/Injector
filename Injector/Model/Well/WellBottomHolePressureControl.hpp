0863ff82eaf6913db72819087789fe3c2ceadc75
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-01-17 16:59:51 +0300

    fix

--- Injector/Model/Well/WellBottomHolePressureControl.hpp
+++ Injector/Model/Well/WellBottomHolePressureControl.hpp
@@ -74,11 +74,6 @@ namespace GPN
                     bc_types[north_id] = BoundaryCondition::BCType::second;
                     bc_types[south_id] = BoundaryCondition::BCType::second;
                 }
-
-                void set_bc_type(const RealType t)
-                {
-                    this->t = t;
-                }
             };
 #pragma endregion
 
