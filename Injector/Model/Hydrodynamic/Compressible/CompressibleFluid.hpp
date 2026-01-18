5a00bc4863a34cd92d15ff02401b223b8adb5f5c
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-01-18 21:09:01 +0300

    fix

--- Injector/Model/Hydrodynamic/Compressible/CompressibleFluid.hpp
+++ Injector/Model/Hydrodynamic/Compressible/CompressibleFluid.hpp
@@ -31,6 +31,7 @@ namespace GPN
             using Base::P_prev;
             using Base::well;
 
+            using functor_type = typename Well_t:: template functor_type<History_t>;
             using hydro_bc_type = typename Well_t::hydro_bc_type;
 
             using Solver_t =
