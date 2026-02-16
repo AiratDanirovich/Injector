b77f98b79dae2746057155849ef4b8c4f1103d2a
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-02-16 12:32:09 +0300

    assert fixed

--- Injector/Properties/Logs.hpp
+++ Injector/Properties/Logs.hpp
@@ -401,7 +401,7 @@ namespace GPN
                 assert(compressibility.size() == is_permeable.size());
                 for (std::ptrdiff_t id{0ll}; id < compressibility.size(); ++id)
                     assert(
-                        ((is_permeable(id) == 1.0) && (compressibility(id) > 0.0)) ||
+                        ((is_permeable(id) == 1.0) && (compressibility(id) >= 0.0)) ||
                         ((is_permeable(id) == 0.0) && (compressibility(id) == 0.0)));
             }
         };
