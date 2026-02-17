467a2526cf3cb14513e0afe8baac96edfadaea02
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-02-17 14:36:12 +0300

    fix tests to check temperature

--- tests/compressible_fluid_field_rate_control_test.cpp
+++ tests/compressible_fluid_field_rate_control_test.cpp
@@ -21,16 +21,21 @@
 
 #include <Injector/Model/Hydrodynamic/Compressible/CompressibleRatesFactory.hpp>
 #include <Injector/Model/Hydrodynamic/Compressible/CompressibleFluid.hpp>
+#include <Injector/Model/Heat/HeatBoundaryConditions.hpp>
 
 #include <Injector/Properties/LogsFactory.hpp>
 #include <Injector/Properties/JT_FieldFactory.hpp>
+#include <Injector/Solver/FullImplicit/Solver.hpp>
+#include <Injector/Solver/SolverManager.hpp>
 
 #include "includes/transfer_to_eigen.hpp"
 #include "includes/make_history.hpp"
+#include "includes/make_geotherma.hpp"
 #include "includes/set_is_permeable_stencils.hpp"
 #include "includes/make_r_stencils.hpp"
 #include "includes/get_completion.hpp"
 #include "includes/make_water.hpp"
+#include "includes/IC_BC.hpp"
 
 #include <nlohmann/json.hpp>
 #include <catch2/catch_test_macros.hpp>
@@ -49,6 +54,8 @@ using namespace GPN::Phases;
 using namespace GPN::Completion;
 using namespace GPN::Hydrodynamic;
 using namespace GPN::Wells::BotHoleRateControl;
+using namespace GPN::EqSolver;
+using namespace GPN::EqSolver::FullImplicit;
 
 using VR = std::vector<GPN::RealType>;
 
@@ -78,7 +85,12 @@ TEST_CASE("Solver", "SelfSimilarCyl")
     const auto RFP_weights_stencils{transfer_to_eigen(data["collector"]["explicit"]["weights"].get<VR>())};
     const auto ext_pressure_stencils{transfer_to_eigen(data["collector"]["external_pressure"].get<VR>(), 1e5)};
     const auto is_perforated_stencils{transfer_to_eigen(data["collector"]["is_perforated"].get<VR>())};
-    const auto is_permeable_stencils{set_is_permeable_stencils(is_perforated_stencils, to_layers)};
+    const auto is_permeable_stencils{set_is_permeable_stencils(is_perforated_stencils, to_layers)};   
+
+    // heat logs
+    const auto solid_heatconductivity_stencils{transfer_to_eigen(data["collector"]["heatConductivity"].get<VR>())};
+    const auto solid_density_stencils{transfer_to_eigen(data["collector"]["solidDensity"].get<VR>())};
+    const auto solid_specific_heatcapacity_stencils{transfer_to_eigen(data["collector"]["solidSpecificHeatCapacity"].get<VR>())};
 
     const auto start_time{data["history"]["start_time"].get<RealType>()};
 
@@ -113,12 +125,17 @@ TEST_CASE("Solver", "SelfSimilarCyl")
         Grids::CylinderGridFactory::create(
             z_refiner, z_stencils,
             r_nodes)};
+    const auto &grid_z{grid2D->first_coord()};
+    const auto &grid_r{grid2D->second_coord()};
 
     const cptr<Grids::CylinderGridRock> grid2D_rocks{
         make_shared<Grids::CylinderGridRock>(grid2D)};
     constexpr auto left_margin{3ll};
     const auto &grid_rocks_z{grid2D_rocks->first_coord()};
     const auto &grid_rocks_r{grid2D_rocks->second_coord()};
+
+    const ExtrudedCasing extr_completion{
+        VarExtrudedCasingFactory::create(completion)};
 
     Logs::Rocks::CoreSampleLogs
         core_logs{
@@ -147,6 +164,30 @@ TEST_CASE("Solver", "SelfSimilarCyl")
             grid2D_rocks};
     const auto &medium_compressibility_field{
         rock_field_props.medium_compressibility};
+    CHECK(medium_compressibility_field.rows() == grid_rocks_z.mesh_size());
+    CHECK(medium_compressibility_field.cols() == grid_rocks_r.mesh_size());
+        
+#pragma region HEAT-SETTINGS
+    const Logs::Rocks::HeatLogs heat_logs{
+        solid_density_stencils,
+        solid_specific_heatcapacity_stencils,
+        solid_heatconductivity_stencils,
+        porosity_stencils,
+        water,
+        grid_z};
+
+    Properties::Rocks::HeatProps heat_props{
+        heat_logs, grid2D};
+    heat_props.apply_well(extr_completion);
+
+    FaceProperties::Rocks::HeatFaceProps heat_face_props{
+        heat_props, grid2D};
+    heat_face_props.apply_well(extr_completion);
+
+    std::unique_ptr<const Logs::Geotherma> geotherma{
+        make_unique<Logs::Geotherma>(
+            make_geotherma(data, grid2D))};
+#pragma endregion
 
 #pragma region MAKE-HISTORY
     const ptr<History> history{make_shared<History>(make_history(data))};
@@ -201,12 +242,43 @@ TEST_CASE("Solver", "SelfSimilarCyl")
     auto &pressure_field{*ptr_pressure_field};
 
     // rates field factory
-    FaceProperties::CompressibleRatesFactory rates_factory{
+    using RatesFactory_t =
+        decltype(FaceProperties::CompressibleRatesFactory{
+            ptr_pressure_field,
+            grid2D_rocks, well, history, water});
+    auto ptr_rates_factory{make_shared<RatesFactory_t>(
         ptr_pressure_field,
-        grid2D_rocks, well, history, water};
+        grid2D_rocks, well, history, water)};
+    RatesFactory_t &rates_factory{*ptr_rates_factory};
 
-    const auto &grid_z{grid2D->first_coord()};
-    const auto &grid_r{grid2D->second_coord()};
+    const auto initial_state{GPN::ICFactory(start_time, grid2D, *geotherma)};
+    // boundary conditions
+    const GPN::Heat::HeatBC bc{
+        grid2D,
+        std::make_shared<GPN::FunctorBC<
+            History,
+            CompressibleFluidField_t,
+            RatesFactory_t>>(
+            history, ptr_rates_factory, *geotherma, grid2D),
+        ptr_rates_factory};
+    // solver
+    using Solver_t = decltype(Solver{
+        heat_face_props.medium_heat_conductivity,
+        grid2D,
+        heat_props.medium_vol_heatcapacity,
+        ptr_rates_factory, initial_state,
+        bc, start_time});
+
+    auto solver_ptr{std::make_shared<Solver_t>(
+        heat_face_props.medium_heat_conductivity,
+        grid2D,
+        heat_props.medium_vol_heatcapacity,
+        ptr_rates_factory, initial_state,
+        bc, start_time)};
+
+    const auto &solver{*solver_ptr};
+
+    SolverManager solver_manager{history, solver_ptr};
 
     //    const auto rMin{grid_r.dual_front()};
     const auto rMax{grid_r.dual_back()};
