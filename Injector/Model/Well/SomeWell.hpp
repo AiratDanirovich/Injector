b353dc30931a0ece5762b2ac991c4511ccd5ccd0
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-02-20 18:28:33 +0300

    fix tests PASS

--- Injector/Model/Well/SomeWell.hpp
+++ Injector/Model/Well/SomeWell.hpp
@@ -27,8 +27,6 @@ namespace GPN
                 Solution1D(const auto history)
                 {
                     times.reserve(history->size());
-                    bottom_pressure.reserve(history->size());
-                    bottom_rate.reserve(history->size());
 
                     rfp.reserve(history->size());
                     wfp.reserve(history->size());
@@ -37,15 +35,40 @@ namespace GPN
                     well_pressure.reserve(history->size());
                 }
 
-                std::vector<RealType> times, bottom_pressure, bottom_rate;
+                std::vector<RealType> times;
                 std::vector<EqSolver::State::State1D>
                     rfp, wfp,
                     verticle_cement_flow,
                     verticle_well_flow,
                     well_pressure;
             };
+            struct SolutionPoint
+            {
+                SolutionPoint(const auto history)
+                    : z_ref{history->z_ref}
+                {
+                    times.reserve(10 * history->size());
+                    bottom_pressure.reserve(10 * history->size());
+                    bottom_rate.reserve(10 * history->size());
+                }
+                std::vector<RealType> times, bottom_pressure, bottom_rate;
+                const RealType z_ref;
+
+                const auto output() const
+                {
+                    Eigen::ArrayXX<RealType> out(static_cast<ptrdiff_t>(times.size()), 3ll);
+                    for(auto row{0ll}; row < out.rows(); ++row)
+                    {
+                        out(row, 0ll) = times[row];
+                        out(row, 1ll) = bottom_pressure[row]/1E5;
+                        out(row, 2ll) = bottom_rate[row];
+                    }
+                    return out;
+                }
+            };
 
             Solution1D solution1D;
+            SolutionPoint solutionPoint;
 
             SomeWell(
                 const Properties::Rocks::RocksProps<Grid2D_t> &
@@ -70,6 +93,7 @@ namespace GPN
                   bottom_pressure{0.0},
                   bottom_rate{0.0},
                   solution1D{history},
+                  solutionPoint{history},
                   well_pressure{StepPropertyContainer::Constant(grid2D_rocks->first_coord().mesh_size(), 0.0)}
             {
                 static_assert(Grid2D_t::l_margin == 3ll);
@@ -110,14 +134,15 @@ namespace GPN
                     if ((t < mid_time) && (t + t_step > mid_time))
                     {
                         solution1D.times.push_back(t + t_step / 2.0);
-                        solution1D.bottom_pressure.push_back(bottom_pressure);
-                        solution1D.bottom_rate.push_back(bottom_rate);
                         solution1D.rfp.emplace_back(RFP());
                         solution1D.wfp.emplace_back(WFP());
                         solution1D.verticle_cement_flow.emplace_back(verticle_cement_flow());
                         solution1D.verticle_well_flow.emplace_back(verticle_well_flow());
                         solution1D.well_pressure.emplace_back(well_pressure);
                     }
+                    solutionPoint.times.push_back(t + t_step);
+                    solutionPoint.bottom_pressure.push_back(bottom_pressure);
+                    solutionPoint.bottom_rate.push_back(bottom_rate);
                 }
 #pragma endregion
             }
