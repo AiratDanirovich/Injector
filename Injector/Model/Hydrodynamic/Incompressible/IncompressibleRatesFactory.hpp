330fe6a3b727927746999875fc6b0aa2e7613ca8
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-02-20 11:02:48 +0300

    compiles, tests NOT pass

--- Injector/Model/Hydrodynamic/Incompressible/IncompressibleRatesFactory.hpp
+++ Injector/Model/Hydrodynamic/Incompressible/IncompressibleRatesFactory.hpp
@@ -20,8 +20,10 @@ namespace GPN
     namespace FaceProperties
     {
         template <
-            typename Grid2D_t, typename Well_t,
-            typename History_t, typename Fluid_t,
+            typename Grid2D_t,
+            typename Well_t,
+            typename History_t,
+            typename Fluid_t,
             typename Hydrodynamics_t>
         struct IncompressibleRatesFactory : public SomeRatesFactory<Grid2D_t, Well_t, History_t, Fluid_t, Hydrodynamics_t>
         {
@@ -29,15 +31,15 @@ namespace GPN
             using Base::get_history_record;
             using Base::history;
             using Base::solution;
+            using Base::well;
 
             IncompressibleRatesFactory(
                 cptr<Hydrodynamics_t> pressure_field,
                 const cptr<Grid2D_t> grid2D_rocks,
-                const Well_t &well,
-                const ptr<History_t> history,
+                const cptr<Well_t> &well,
+                const cptr<History_t> history,
                 const Fluid_t &fluid)
-                : Base{pressure_field, grid2D_rocks, well, history, fluid},
-                  fluid{fluid},
+                : Base{pressure_field, grid2D_rocks, well, history, fluid}
             {
             }
 
@@ -47,54 +49,14 @@ namespace GPN
             void set_flow_field(
                 double t, RealType t_step)
             {
+                const auto& rfp{well->rfp};
                 Base::set_flow_field(
                     t, t_step,
-                    [&well->rfp](const ptrdiff_t /*count*/, const ptrdiff_t /*col*/)
+                    [&rfp](const ptrdiff_t /*count*/, const ptrdiff_t /*col*/)
                     {
-                        return well->rfp
+                        return rfp;
                     });
             }
-
-            const auto get_spatial_JT_contribution() const
-            {
-                return Properties::JT_FieldFactory::create_spatial(*this).values();
-            }
-
-            const auto get_temporal_JT_contribution() const
-            {
-                throw std::runtime_error("The temporal JT contribution was not tested for Incompressible fluid.");
-                //    return Properties::JT_FieldFactory::create_temporal(*this).values();
-            }
-
-            const auto get_heat_flow_in_axes1() const
-            {
-                return heat_flow_field->axes1_as_face_normal_pos + heat_flow_field->axes1_as_face_normal_neg;
-            }
-            const auto get_heat_flow_in_axes2() const
-            {
-                return heat_flow_field->axes2_as_face_normal_pos + heat_flow_field->axes2_as_face_normal_neg;
-            }
-
-            const auto &get_heat_flow_in_axes1_pos() const
-            {
-                return heat_flow_field->axes1_as_face_normal_pos;
-            }
-            const auto &get_heat_flow_in_axes2_pos() const
-            {
-                return heat_flow_field->axes2_as_face_normal_pos;
-            }
-            const auto &get_heat_flow_in_axes1_neg() const
-            {
-                return heat_flow_field->axes1_as_face_normal_neg;
-            }
-            const auto &get_heat_flow_in_axes2_neg() const
-            {
-                return heat_flow_field->axes2_as_face_normal_neg;
-            }
-
-        protected:
-            cptr<FaceProperties::HeatFlowField> heat_flow_field;
-            cptr<FaceProperties::ReservoirFlowField> volumetric_flow_field;
         };
 
         template <typename Grid2D_t, typename Fluid_t>
