c5505347b07c57b7bf6aa5112230dd4b21b8b7af
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-02-20 13:48:28 +0300

    refactored

--- Injector/Model/Well/WellBottomHoleRateControl.hpp
+++ Injector/Model/Well/WellBottomHoleRateControl.hpp
@@ -124,6 +124,8 @@ namespace GPN
                 : public CrossFlow_t,
                   public DefaultWellNumerics<1ll>
             {
+                using Base = CrossFlow_t;
+
                 using functor_type = BotHoleRateFunctorBC<Fluid_t, History_t, Grid2D_t>;
                 using hydro_bc_type = BotHoleRateBC;
                 using grid_type = Grid2D_t;
@@ -184,22 +186,22 @@ namespace GPN
                 template <typename HistoryRecord_t>
                 const auto get_RFP(const HistoryRecord_t &record) const
                 {
-                    return this->rfp;
+                    return Base::rfp;
                 }
                 template <typename HistoryRecord_t>
                 const auto get_WFP(const HistoryRecord_t &record) const
                 {
-                    return this->wfp;
+                    return Base::wfp;
                 }
                 template <typename HistoryRecord_t>
                 const auto get_verticle_cement_flow(const HistoryRecord_t &record) const
                 {
-                    return this->verticle_flux_in_cement;
+                    return Base::verticle_flux_in_cement;
                 }
                 template <typename HistoryRecord_t>
                 const auto get_verticle_well_flow(const HistoryRecord_t &record) const
                 {
-                    return this->verticle_flux_in_well;
+                    return Base::verticle_flux_in_well;
                 }
 
                 template <typename HistoryRecord_t>
